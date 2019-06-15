291a4ff3c45fcb993be7f76de572a8bc1f724511
bigI <chenjianai@126.com>
2019-06-15 14:04:03 +0800

    压缩加密

--- app/src/main/java/com/big/simplecash/util/Utils.java
+++ app/src/main/java/com/big/simplecash/util/Utils.java
@@ -1,13 +1,17 @@
 package com.big.simplecash.util;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
+
+import android.util.Base64;
 
 /**
  * Created by big on 2019/6/12.
@@ -43,7 +47,8 @@ public class Utils {
             gzip.write(str.getBytes());
             gzip.close();
             // 使用指定的 charsetName，通过解码字节将缓冲区内容转换为字符串
-            return out.toString("ISO-8859-1");
+//            return out.toString("ISO-8859-1");
+            return Base64.encodeToString(out.toByteArray(),Base64.NO_WRAP);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -62,11 +67,11 @@ public class Utils {
             return str;
         }
         try {
+//            str=decode(str);
             // 创建一个新的 byte 数组输出流
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             // 创建一个 ByteArrayInputStream，使用 buf 作为其缓冲区数组
-            ByteArrayInputStream in = new ByteArrayInputStream(str
-                .getBytes("ISO-8859-1"));
+            ByteArrayInputStream in = new ByteArrayInputStream(Base64.decode(str, Base64.NO_WRAP));
             // 使用默认缓冲区大小创建新的输入流
             GZIPInputStream gzip = new GZIPInputStream(in);
             byte[] buffer = new byte[256];
@@ -82,4 +87,34 @@ public class Utils {
         }
         return str;
     }
+
+    /**
+     * 加密
+     * oldWord：需要加密的文字/比如密码
+     */
+    public static String encode(String oldWord) {
+        String encodeWord = "";
+        try {
+            encodeWord = Base64.encodeToString(oldWord.getBytes("utf-8"), Base64.NO_WRAP);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encodeWord;
+    }
+
+    /**
+     * 解密
+     * encodeWord：加密后的文字/比如密码
+     */
+    public static String decode(String encodeWord) {
+        String decodeWord = "";
+        try {
+            decodeWord = new String(Base64.decode(encodeWord, Base64.NO_WRAP), "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return decodeWord;
+
+    }
+
 }
